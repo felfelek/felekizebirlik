9c29f47075af895acf52120ec4d6afa325c3bf92
ayseg <aysegul.dalkiran.98@gmail.com>
2019-05-07 16:49:06 +0300

    add drawables

---
+++ app/src/main/java/com/lecboxyazilim/lecbox/FenEdebiyatActivity.java
@@ -0,0 +1,13 @@
+package com.lecboxyazilim.lecbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FenEdebiyatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fen_edebiyat);
+    }
+}
